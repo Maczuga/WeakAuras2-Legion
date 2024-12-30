4d325e068b5effeaeaf2d6af7d4502d08c4a9c4d
Łukasz K <maczugapl@gmail.com>
2024-12-30 14:38:41 +0100

    More compat files rearrangement

---
+++ WeakAurasLegionCompat/Globals/Lua/ipairs_reverse.lua
@@ -0,0 +1,12 @@
+if not ipairs_reverse then
+    function ipairs_reverse(t)
+        local function reverse_iterator(t, i)
+            i = i - 1 -- Decrement the index
+            if i > 0 then
+                return i, t[i] -- Return the current index and value
+            end
+        end
+
+        return reverse_iterator, t, #t + 1 -- Start from one past the last index
+    end
+end
