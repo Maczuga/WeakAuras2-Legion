eb4077253a7c7e8722d0a3b80650b15c41c7b6a1
Łukasz K <maczugapl@gmail.com>
2025-02-28 14:55:14 +0100

    Use first 8.0.1 atlas files for now until I figure out if it's possible to convert the script to 7.3.5 data format.

--- .github/scripts/update-atlas-files.sh
+++ .github/scripts/update-atlas-files.sh
@@ -2,8 +2,8 @@
 
 for version in wow
 do
-  wget -O "UiTextureAtlas.csv" "https://wago.tools/db2/UiTextureAtlas/csv?build=7.3.5.26972"
-  wget -O "UiTextureAtlasMember.csv" "https://wago.tools/db2/UiTextureAtlasMember/csv?build=7.3.5.26972"
+  wget -O "UiTextureAtlas.csv" "https://wago.tools/db2/UiTextureAtlas/csv?build=8.0.1.26367"
+  wget -O "UiTextureAtlasMember.csv" "https://wago.tools/db2/UiTextureAtlasMember/csv?build=8.0.1.26367"
   wget -O "UiTextureAtlasElement.csv" "https://wago.tools/db2/UiTextureAtlasElement/csv?build=8.0.1.26367"
 
   lua ./atlas_update.lua ${version}
