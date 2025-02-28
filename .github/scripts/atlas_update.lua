c734d9748e0a990863d41aed111f1e8f1d6af674
Łukasz K <maczugapl@gmail.com>
2025-02-28 15:00:38 +0100

    7.3.5 attempt

--- .github/scripts/atlas_update.lua
+++ .github/scripts/atlas_update.lua
@@ -26,21 +26,21 @@ for lineNr, atlas in ftcsv.parseLine("UiTextureAtlas.csv", ",") do
 end
 
 for lineNr, member in ftcsv.parseLine("UiTextureAtlasMember.csv", ",") do
+  local name = member.CommittedName
   local uiTextureAtlasID = member.UiTextureAtlasID
-  local uiTextureAtlasElementId = member.UiTextureAtlasElementID
 
-  if uiTextureAtlasElementId and validAtlasIds[uiTextureAtlasID] and not validAtlasElementIds[uiTextureAtlasElementId] then
-    validAtlasElementIds[uiTextureAtlasElementId] = true
-  end
-end
-
-for lineNr, element in ftcsv.parseLine("UiTextureAtlasElement.csv", ",") do
-  local name = element.Name
-  local id = element.ID
-  if validAtlasElementIds[id] and name:lower():sub(1, 5) ~= "cguy_" then
+  if validAtlasIds[uiTextureAtlasID] then
     validNames[name] = true
   end
 end
+
+-- for lineNr, element in ftcsv.parseLine("UiTextureAtlasElement.csv", ",") do
+--   local name = element.Name
+--   local id = element.ID
+--   if validAtlasElementIds[id] and name:lower():sub(1, 5) ~= "cguy_" then
+--     validNames[name] = true
+--   end
+-- end
 
 --for name in pairs(validNames) do
 --  print("Found +", name)
