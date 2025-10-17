1155c0ede50d9b03cb6931dd588f6ac07eeab9d7
Łukasz K. <maczugapl@gmail.com>
2025-10-17 20:50:52 +0200

    Some C_Spell backports Somehow I forgot to backport C_Spell.GetSpellInfo - this might help modernizing several old weakauras. Also GetSpellIDForSpellIdentifier

--- WeakAurasLegionCompat/API/C_Spell.lua
+++ WeakAurasLegionCompat/API/C_Spell.lua
@@ -1,6 +1,37 @@
 if not C_Spell then
   C_Spell = {
     GetSpellInfo = function (spellIdentifier)
+      local name, rank, icon, castTime, minRange, maxRange, returnedSpellID = GetSpellInfo(spellIdentifier)
+      if not name then
+        return nil
+      end
+
+      local spellInfo = {
+        name = name,
+        iconID = icon,
+        castTime = castTime,
+        minRange = minRange,
+        maxRange = maxRange,
+        originalIconID = icon,
+      }
+
+      if type(spellIdentifier) == "number" then
+        spellInfo.spellID = spellIdentifier
+      else
+        spellInfo.spellID = returnedSpellID
+      end
+
+      if returnedSpellID then
+        local baseSpellID = FindBaseSpellByID(returnedSpellID)
+        if baseSpellID and baseSpellID ~= returnedSpellID then
+          local originalIcon = GetSpellTexture(baseSpellID)
+          if originalIcon then
+            spellInfo.originalIconID = originalIcon
+          end
+        end
+      end
+
+      return spellInfo
     end,
     GetSpellName = function (spellIdentifier)
       return select(1, GetSpellInfo(spellIdentifier))
@@ -28,5 +59,28 @@ if not C_Spell then
     RequestLoadSpellData = function(spellId)
       GetSpellInfo(spellId)
     end,
+    GetSpellIDForSpellIdentifier = function(spellIdentifier)
+      if type(spellIdentifier) == "number" then
+        return spellIdentifier
+      elseif type(spellIdentifier) == "string" then
+        local spellId
+        -- Check if it's a spell link
+        local linkMatch = string.match(spellIdentifier, "|Hspell:(%d+)")
+        if linkMatch then
+          spellId = tonumber(linkMatch)
+        else
+          -- It's a spell name, get its base ID
+          spellId = select(7, GetSpellInfo(spellIdentifier))
+          if spellId then
+            -- Check for an override
+            spellId = FindSpellOverrideByID(spellId)
+          end
+        end
+
+        if spellId and GetSpellInfo(spellId) then
+          return spellId
+        end
+      end
+    end
   }
 end
